ca65e64c29d0d1352bfad980dc361e04e0c78ffb
Samuel Adenuga <michael_adenuga@Samuels-MacBook-Pro-2.local>
2014-12-04 13:40:12 +0000

    Initial Commit

---
+++ Christmas Exercise/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Christmas Exercise
+//
+//  Created by Samuel Adenuga on 04/12/2014.
+//  Copyright (c) 2014 Samuel Adenuga. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
